09ad5a9caee7275d223aad4e76cab99ab4f53806
Jonathan <johnypatino99@gmail.com>
2023-11-16 18:56:11 -0500

    css changes + about me section started

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+/*-------------- NavBar script ----------------*/
 const toggleMenu = document.getElementsByClassName('toggle-menu')[0]
 const navbarlinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleMenu.addEventListener('click',()=> {
     navbarlinks.classList.toggle('active')
 })
+
+
 

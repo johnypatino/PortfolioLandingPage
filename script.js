8ee7785edbef72c21882692a5bc5e5f00402edf9
Jonathan <johnypatino99@gmail.com>
2023-11-15 19:16:11 -0500

    navbar toggle

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggleMenu = document.getElementsByClassName('toggle-menu')[0]
+const navbarlinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleMenu.addEventListener('click',()=> {
+    navbarlinks.classList.toggle('active')
+})
+
